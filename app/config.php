5f625514657d7e70fb305b5c429e78612a41cf91
Gabriel Lima dos Santos <gabriel.amo.jmbb@gmail.com>
2026-01-22 16:33:56 -0300

    Atualiza URL CSV da planilha

--- app/config.php
+++ app/config.php
@@ -1,8 +1,8 @@
 <?php
 // app/config.php
 return [
-  'CSV_ENGAGEMENTS' => 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQRb4iqvnPTl9Rnx3RpC71u90Lr2zFUQJDXePfPoqcDOnyxOwpAyftR8Eaw2BT25Q/pub?gid=1904854855&single=true&output=csv',
-  'CSV_RECEIPTS'    => 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQRb4iqvnPTl9Rnx3RpC71u90Lr2zFUQJDXePfPoqcDOnyxOwpAyftR8Eaw2BT25Q/pub?gid=22760259&single=true&output=csv',
+  'CSV_ENGAGEMENTS' => 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ32JUumr5Ahzw3QozKCpZnMJP2ujsaTjCmdI8ktPTKt17oUtMRZs_nPIVtwr0KJFWeBwhIGxh_3zkr/pub?output=csv',
+  'CSV_RECEIPTS'    => 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ32JUumr5Ahzw3QozKCpZnMJP2ujsaTjCmdI8ktPTKt17oUtMRZs_nPIVtwr0KJFWeBwhIGxh_3zkr/pub?output=csv',
 
   'ADMIN_EMAIL'     => 'admin@jml.local',
   'ADMIN_PASS_HASH' => '$2y$10$sScGiHVTh.lC/4xiZdpIt.VbQXC18DNdL.hnwxD9Hqckm.KAvlX02',
